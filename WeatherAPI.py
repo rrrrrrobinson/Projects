670520417315729a399fc90696211ad28d02ecd4
Hong Yang Chen <hchen88@syr.edu>
2021-04-23 13:13:57 -0400

    a

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -3,29 +3,56 @@ import json
 import requests
 import datetime
 
+#api key from openweather 
 api_key = "886f6a4c98d1b377418e02affd7ede7a"
+
+#lat/long of Syracuse
 lat = 43.0481
 long = -76.1474
 
-date = datetime.datetime.now()
+#to record date and time of when data were collect
+date_time = datetime.datetime.now()
 
-def weather_data_api(aKey, aLat, aLong):  
-    url = "http://api.openweathermap.org/data/2.5/weather?lat="+ str(aLat) + "&lon=" + str(aLong) + "&appid=" + aKey + "&units=imperial"
-    response = requests.get(url,  auth=('user', 'pass'))
+#to round seconds to then nearest whole number
+#cast date_time to str
+item = str(date_time)
 
-    data = response.json()
+#take out date and time seperately and split hour, mintues and second
+date = item.split()[0]
+time = item.split()[1]
+hours = time.split(':')[0]
+mins = time.split(':')[1]
 
-    if data["cod"] != "404":
-        weather = data["weather"]
-        main = data["main"]
-        wind = data["wind"]
-        print("Weather: " + weather[0]['description'])
-        print("Temperture(Fahrenheit): " + str(round(main['temp'])) + '(H:' + str(round(main['temp_max']))+ '\L:' + str(round(main['temp_min'])) + ')')
-        print("Wind Speed(MPH): " + str(wind['speed']))
-        
-    else:
-        print("404 City Not Found")
+#round the second
+secs = round(float(time.split(':')[2]))
 
 
-print(date)    
-weather_data_api(api_key, lat, long)
+
+#url for the http request
+#this is current weather apu
+url = "http://api.openweathermap.org/data/2.5/weather?lat="+ str(lat) + "&lon=" + str(long) + "&appid=" + api_key + "&units=imperial"
+response = requests.get(url,  auth=('user', 'pass'))
+    
+#store json object
+data = response.json()
+
+#check wether the location exist
+if data["cod"] != "404":
+
+    #return get specific value from json object
+    weather = data["weather"]
+    main = data["main"]
+    wind = data["wind"]
+     
+else:        
+    print("404 City Not Found")
+
+
+#print datas need
+print("Date: " + date)
+print("Time: " + hours + ':' + mins + ':' + str(secs))
+print("Weather: " + weather[0]['description'])
+print("Temperture(Fahrenheit): " + str(round(main['temp'])) + '(H:' + str(round(main['temp_max']))+ '\L:' + str(round(main['temp_min'])) + ')')
+print("Wind Speed(MPH): " + str(wind['speed']))
+
+   
