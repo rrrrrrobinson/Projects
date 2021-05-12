4b8ecdad3cf4c29ebdcb3af42e9b55967af98274
Michael Jones <mwjones@syr.edu>
2021-05-11 20:35:20 -0400

    weather

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -61,21 +61,24 @@ def getWeatherData():
     # print("Wind Speed(MPH): " + str(wind_speed))
     # print(' ')
     
-    #determine wheather it is a good or bad weather
+    #determine weather
+    # < 800 accounts for rain, snow, thundrstorms, and all weather types in between
     if (weather_id < 800):
         condition = 'BAD'
-
+    # > 800 and  < 803 accounts for clear sky and partly cloudy
     elif (weather_id > 800 and weather_id < 803):
+        # temperature >= 45 and <= 80 and wind < 15 mph
         if (average_temp >= 45 and average_temp <= 80 and wind_speed <= 15):
             condition = 'GOOD'
-            
+        # temperature < 45 and > 80 and wind > 15 mph   
         elif (average_temp < 45 or average_temp > 80 or wind_speed > 15):
             condition = 'BAD'
-
+    # heavy clouds and completely cloudy
     elif (weather_id >= 803):
+        # temperature >= 45 and <= 80 and wind <= 15 mph
         if (average_temp >= 45 and average_temp <= 80 and wind_speed <= 15):
             condition = 'OK'
-            
+        # temperature < 45 and > 80 and wind > 15 mph    
         elif (average_temp < 45 or average_temp > 80 or wind_speed > 15):
             condition = 'BAD'
 
