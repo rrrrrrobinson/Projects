a161047a10e97475faa831a3b8fb972d3ab57ed8
Michael Jones <mwjones@syr.edu>
2021-05-16 13:22:40 -0400

    dbpy

--- DbPy.py
+++ DbPy.py
@@ -2,20 +2,21 @@
 from pymongo import MongoClient
 import Cookbook
 
+# create mongoDb client object for database interaction
 client = MongoClient("mongodb+srv://user0:gKGDWJlHy6qGZrhx@weathermentsdb.tdbnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
+# choose the database we want to connect to 
 db = client.get_database('WeathermentsDB')
-dbTweets = db.TweetData # tweetData collection
+# tweetData collection
+dbTweets = db.TweetData 
 
+# Save a tweet object to the databse we established above
 def saveToDB(tweet):
+    # add tweetobject to remote database after transforming into a dicitonary
     dbTweets.insert_one(tweet.__dict__)
     return "tweet collected"
 
-def saveToDBWithWeather(tweet):
-    tweet.setWeath(WeatherAPI.getWeatherData())
-    dbTweets.insert_one(tweet.__dict__)
-
+# Retreive all tweets from database that match the input parameter for weather type
 def pullByWeather(sent):
-    a = dbTweets.find({"weatherSent" : sent})
-    return a
+    return dbTweets.find({"weatherSent" : sent})
 
